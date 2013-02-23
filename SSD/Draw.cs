b83a56ef43e2316ea5679d09cbf23f6b13ba5b69
Yoiink <ll_lightning_ll@hotmail.co.uk>
2013-02-23 12:04:31 +0000

    Initial Commit
    Currently, the project contains some models and the general idea of how
    the player will move around the world.
    
    Although I'm not concentrating too much on the final game now as I
    should cover the milestones first.

---
+++ SSD/Draw.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+
+namespace SSD
+{
+    class Draw
+    {
+
+        public Draw(GraphicsDevice graphicsDevice, ContentManager content)
+        {
+            _graphicsDevice = graphicsDevice;
+            _content = content;
+            _models = new Dictionary<string,ModelContainer>();
+            //Matrix view = Matrix.CreateLookAt(playerShip.getMatrix().Translation + (playerShip.getMatrix().Up * 100), playerShip.getMatrix().Translation, playerShip.getMatrix().Left);
+            //Matrix proj = Matrix.CreatePerspectiveFieldOfView(MathHelper.PiOver4, aspectRatio, 1.0f, 10000.0f);
+
+        }
+
+        public void render(ModelContainer model, Matrix view, Matrix proj)
+        {
+            foreach (ModelMesh mesh in model.getModel().Meshes)
+            {
+                foreach (BasicEffect effect in mesh.Effects)
+                {
+                    effect.EnableDefaultLighting();
+                    effect.PreferPerPixelLighting = true;
+
+                    effect.World = model.getBoneTransform(mesh.ParentBone.Index) * model.getMatrix();
+                    effect.View = view;
+                    effect.Projection = proj;
+                }
+                mesh.Draw();
+            }
+        }
+
+        public void renderAllModels(Matrix view, Matrix proj)
+        {
+            foreach (KeyValuePair<string, ModelContainer> modelEntry in _models)
+            {
+                foreach (ModelMesh mesh in modelEntry.Value.getModel().Meshes)
+                {
+                    foreach (BasicEffect effect in mesh.Effects)
+                    {
+                        effect.EnableDefaultLighting();
+                        effect.PreferPerPixelLighting = true;
+
+                        effect.World = modelEntry.Value.getBoneTransform(mesh.ParentBone.Index) * modelEntry.Value.getMatrix();
+                        effect.View = view;
+                        effect.Projection = proj;
+                    }
+                    mesh.Draw();
+                }
+            }
+        }
+
+        public void addModel(String modelName, String filePath)
+        {
+            _models.Add(modelName, new ModelContainer(filePath, _content));
+        }
+
+        public ModelContainer getModel(String modelName)
+        {
+            try
+            {
+                ModelContainer model;
+                _models.TryGetValue(modelName, out model);
+                return model;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+                throw;
+            }
+        }
+
+        GraphicsDevice _graphicsDevice;
+        ContentManager _content;
+        //List<ModelContainer> _models;
+        Dictionary<string, ModelContainer> _models;
+    }
+}
---
+++ SSD/Game1.cs
@@ -0,0 +1,255 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+
+namespace SSD
+{
+    /// <summary>
+    /// This is the main type for your game
+    /// </summary>
+    public class Game1 : Microsoft.Xna.Framework.Game
+    {
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+
+        //ModelContainer playerShip;
+        //ModelContainer worldModel;
+        //ModelContainer spaceSphere;
+
+        float aspectRatio;
+        //float playerSpeed;
+
+        Draw _renderer;
+        
+
+        //ModelContainer myModel;
+
+        // Set the position of the model in world space, and set the rotation.
+        //Matrix shipMatrix = Matrix.Identity;
+        //Matrix worldMatrix = Matrix.Identity;
+
+        // Set the position of the camera in world space, for our view matrix.
+        Matrix cameraPosition = Matrix.Identity;
+
+        //Camera position
+        Vector3 cameraThirdPerson = new Vector3(0, 200, -200);
+
+        public Game1()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            
+        }
+
+        /// <summary>
+        /// Allows the game to perform any initialization it needs to before starting to run.
+        /// This is where it can query for any required services and load any non-graphic
+        /// related content.  Calling base.Initialize will enumerate through any components
+        /// and initialize them as well.
+        /// </summary>
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+            Window.Title = "Super Stardust Clone";
+            base.Initialize();
+
+            //_renderer = new Draw(graphics.GraphicsDevice, Content);
+        }
+
+        /// <summary>
+        /// LoadContent will be called once per game and is the place to load
+        /// all of your content.
+        /// </summary>
+        protected override void LoadContent()
+        {
+            // Create a new SpriteBatch, which can be used to draw textures.
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            //Setup the game window
+            graphics.PreferredBackBufferWidth = 1280;
+            graphics.PreferredBackBufferHeight = 720;
+            graphics.IsFullScreen = false;
+            graphics.PreferMultiSampling = true;
+            graphics.ApplyChanges();
+
+
+            _renderer = new Draw(graphics.GraphicsDevice, Content);
+
+            //Load model
+            //playerShip = Content.Load<Model>("Models\\space_frigate");
+            //worldModel = Content.Load<Model>("Models\\burning_planet");
+
+            _renderer.addModel("playerShip", "Models\\space_frigate");
+            _renderer.addModel("worldSphere", "Models\\burning_planet");
+            _renderer.addModel("spaceSphere", "Models\\space_sphere");
+            _renderer.addModel("e_one", "Models\\e_one");
+
+            //playerShip = new ModelContainer("Models\\space_frigate");
+            //worldModel = new ModelContainer("Models\\burning_planet");
+            //spaceSphere = new ModelContainer("Models\\space_sphere");
+
+            aspectRatio = graphics.GraphicsDevice.Viewport.AspectRatio;
+
+            _renderer.getModel("e_one").setScale(1f);
+            _renderer.getModel("e_one").setPosition(0, 3500f, 0);
+
+            _renderer.getModel("playerShip").setScale(10f);
+            _renderer.getModel("playerShip").setPosition(0, 6000f, 0);
+
+            _renderer.getModel("spaceSphere").setScale(100f);
+            //spaceSphere.setPosition(0, -6000f, 0);
+
+            //shipMatrix *= Matrix.CreateScale(20f);
+            //shipMatrix *= Matrix.CreateTranslation(0, 1500f, 0);
+
+            _renderer.getModel("worldSphere").setScale(40f); //40
+            _renderer.getModel("worldSphere").addPitch(90);
+
+            //worldMatrix *= Matrix.CreateScale(40f);
+            //worldMatrix *= Matrix.CreateRotationX(MathHelper.ToRadians(90));
+
+            //myModel = new ModelContainer("Models\\space_frigate", Content);
+            
+
+        }
+
+        /// <summary>
+        /// UnloadContent will be called once per game and is the place to unload
+        /// all content.
+        /// </summary>
+        protected override void UnloadContent()
+        {
+            // TODO: Unload any non ContentManager content here
+        }
+
+        /// <summary>
+        /// Allows the game to run logic such as updating the world,
+        /// checking for collisions, gathering input, and playing audio.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Update(GameTime gameTime)
+        {
+            #region Controls
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed)
+                this.Exit();
+
+            if (GamePad.GetState(PlayerIndex.One).ThumbSticks.Left.X != 0 || GamePad.GetState(PlayerIndex.One).ThumbSticks.Left.Y != 0)
+            {
+
+
+                //playerShip.applyMatrix(Matrix.CreateFromAxisAngle(Vector3.Normalize(playerShip.getMatrix().Right), (GamePad.GetState(PlayerIndex.One).ThumbSticks.Left.X * -0.1f) / 10));
+                //playerShip.applyMatrix(Matrix.CreateFromAxisAngle(Vector3.Normalize(playerShip.getMatrix().Forward), (GamePad.GetState(PlayerIndex.One).ThumbSticks.Left.Y * -0.1f) / 10));
+
+
+                ModelContainer worldModel = _renderer.getModel("worldSphere");
+                ModelContainer spaceSphere = _renderer.getModel("spaceSphere");
+                ModelContainer playerShip = _renderer.getModel("playerShip");
+
+                worldModel.applyMatrix(Matrix.CreateFromAxisAngle(Vector3.Normalize(playerShip.getMatrix().Left), (GamePad.GetState(PlayerIndex.One).ThumbSticks.Left.X * -0.1f) / 10));
+                worldModel.applyMatrix(Matrix.CreateFromAxisAngle(Vector3.Normalize(playerShip.getMatrix().Backward), (GamePad.GetState(PlayerIndex.One).ThumbSticks.Left.Y * -0.1f) / 10));
+
+                spaceSphere.applyMatrix(Matrix.CreateFromAxisAngle(Vector3.Normalize(playerShip.getMatrix().Right), (GamePad.GetState(PlayerIndex.One).ThumbSticks.Left.X * -0.1f) / 50));
+                spaceSphere.applyMatrix(Matrix.CreateFromAxisAngle(Vector3.Normalize(playerShip.getMatrix().Forward), (GamePad.GetState(PlayerIndex.One).ThumbSticks.Left.Y * -0.1f) / 50));
+
+                //worldModel.addPitch(GamePad.GetState(PlayerIndex.One).ThumbSticks.Left.Y);
+                //worldModel.addYaw(GamePad.GetState(PlayerIndex.One).ThumbSticks.Left.Y);
+                //worldModel.addRoll(GamePad.GetState(PlayerIndex.One).ThumbSticks.Left.Y);
+
+            }
+
+            if (GamePad.GetState(PlayerIndex.One).ThumbSticks.Right.X != 0 || GamePad.GetState(PlayerIndex.One).ThumbSticks.Right.Y != 0)
+            {
+
+                //playerShip.addPitch(GamePad.GetState(PlayerIndex.One).ThumbSticks.Right.X);
+                //playerShip.addRoll(GamePad.GetState(PlayerIndex.One).ThumbSticks.Right.Y);
+                double angle = Math.Atan2(GamePad.GetState(PlayerIndex.One).ThumbSticks.Right.Y * -1, GamePad.GetState(PlayerIndex.One).ThumbSticks.Right.X * -1);
+                Console.WriteLine(angle);
+
+                _renderer.getModel("playerShip").setYaw((MathHelper.ToDegrees((float)angle) - 90));
+
+                //modelRotation.Y -= (float)gameTime.ElapsedGameTime.TotalMilliseconds * MathHelper.ToRadians(0.1f) * GamePad.GetState(PlayerIndex.One).ThumbSticks.Right.X;
+                //modelRotation.X -= (float)gameTime.ElapsedGameTime.TotalMilliseconds * MathHelper.ToRadians(0.1f) * GamePad.GetState(PlayerIndex.One).ThumbSticks.Right.Y;
+
+                //shipMatrix *= Matrix.CreateTranslation(shipMatrix.Left * GamePad.GetState(PlayerIndex.One).ThumbSticks.Right.Y);
+
+                //playerShip.applyMatrix(Matrix.CreateTranslation(playerShip.getMatrix().Left * GamePad.GetState(PlayerIndex.One).ThumbSticks.Right.Y));
+            }
+
+            #endregion
+
+            //modelRotation += (float)gameTime.ElapsedGameTime.TotalMilliseconds * MathHelper.ToRadians(0.1f);
+
+            //Rotate the planet
+            //worldMatrix *= Matrix.CreateRotationY(MathHelper.ToRadians(0.1f));
+            //worldMatrix *= Matrix.CreateRotationZ(MathHelper.ToRadians(0.05f));
+
+            _renderer.getModel("worldSphere").addRoll(0.1f);
+            _renderer.getModel("worldSphere").addYaw(0.05f);
+
+            _renderer.getModel("spaceSphere").addRoll(-0.1f);
+            _renderer.getModel("spaceSphere").addYaw(-0.05f);
+
+            _renderer.getModel("e_one").addYaw(1);
+            _renderer.getModel("e_one").applyMatrix(Matrix.CreateFromAxisAngle(Vector3.Normalize(_renderer.getModel("e_one").getMatrix().Backward), 
+                                                    0.01f));
+
+
+            //playerShip.addYaw(1);
+
+            Quaternion cameraOriantaion = Quaternion.CreateFromRotationMatrix(_renderer.getModel("playerShip").getMatrix());
+            cameraPosition = Matrix.CreateTranslation(new Vector3(0, 1000, -1000) + _renderer.getModel("playerShip").getPosition());
+
+            base.Update(gameTime);
+        }
+
+        /// <summary>
+        /// This is called when the game should draw itself.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Draw(GameTime gameTime)
+        {
+
+            /* DISABLE CULLING - Useful to see backwards triangles. 
+            RasterizerState rs = new RasterizerState();
+            rs.CullMode = CullMode.None;
+            //rs.FillMode = FillMode.WireFrame;
+            graphics.GraphicsDevice.RasterizerState = rs;*/
+
+            //Console.WriteLine(GraphicsDevice.Adapter.DeviceName.ToString());
+
+            GraphicsDevice.Clear( ClearOptions.Target | ClearOptions.DepthBuffer, Color.CornflowerBlue, 1.0f, 0);
+
+            // Copy any parent transforms. (From bones)
+
+            //Matrix[] transforms = new Matrix[myModel.getModel().Bones.Count];
+            //myModel.getModel().CopyAbsoluteBoneTransformsTo(transforms);            
+
+            graphics.GraphicsDevice.DepthStencilState = DepthStencilState.Default;
+
+            Viewport viewport = graphics.GraphicsDevice.Viewport;
+            float aspectRatio = viewport.AspectRatio;
+
+            //Matrix view = Matrix.CreateLookAt(playerShip.getMatrix().Translation + (playerShip.getMatrix().Up * 500), playerShip.getMatrix().Translation, playerShip.getMatrix().Left);
+            Matrix view = Matrix.CreateLookAt(_renderer.getModel("playerShip").getMatrix().Translation + (_renderer.getModel("playerShip").getMatrix().Up * 500),
+                                                _renderer.getModel("playerShip").getMatrix().Translation, new Vector3(1, 0, 0));
+            Matrix proj = Matrix.CreatePerspectiveFieldOfView(MathHelper.PiOver4, aspectRatio, 1.0f, 50000.0f);
+
+            //Render all models that are loaded in.
+            _renderer.renderAllModels(view, proj);
+
+            //Draw 2D things
+            spriteBatch.Begin();
+            //Draw nothing
+            spriteBatch.End();
+
+            base.Draw(gameTime);
+        }
+    }
+}
